d5c6f57dea85f2680a421abf6ef9dd54ea04b493
Oliani <oliani748@gmail.com>
2026-01-21 15:35:20 -0300

    create project

---
+++ backend/machineTelemetry/src/main/java/com/machineTelemetry/MachineTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.machineTelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineTelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineTelemetryApplication.class, args);
+	}
+
+}
---
+++ backend/machineTelemetry/src/test/java/com/machineTelemetry/MachineTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machineTelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
